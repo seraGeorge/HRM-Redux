ef70c34905ac715fcbdf1c244321282c777fbf4c
Ashwin Nair <ashwin.nair@qburst.com>
2023-12-12 11:35:22 +0530

    [edit]:Added cards

--- src/core/constants/employmentModes.ts
+++ src/core/constants/employmentModes.ts
@@ -1,4 +1,14 @@
-const employmentModes = {
-  name: "Full time",
-};
+const employmentModes = [
+  { id: 1, mode: "Full-Time" },
+  { id: 2, mode: "Part-Time" },
+  { id: 3, mode: "Contract" },
+  { id: 4, mode: "Freelance" },
+  { id: 5, mode: "Temporary" },
+  { id: 6, mode: "Internship" },
+  { id: 7, mode: "Full-time" },
+  { id: 8, mode: "Remote" },
+  { id: 9, mode: "Permanent" },
+  { id: 10, mode: "Consultant" },
+];
+
 export default employmentModes;
---
+++ src/pages/EmployeeListing/EmployeeCards/Card/Card.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Employee } from "../../../../core/interfaces/interface";
+
+const Card = ({ employee }: { employee: Employee }) => {
+  return <div>Card</div>;
+};
+
+export default Card;
